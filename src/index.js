b114835c5e20348caa7beae352e2033be64e648a
AndreyHerzhov <andreyherzhov@gmail.com>
2022-08-05 16:22:45 +0300

    adaptive

--- src/index.js
+++ src/index.js
@@ -1,67 +1,102 @@
 const refs = {
     addBtn: document.querySelector('.add_button'),
     input: document.querySelector('input'),
-    list: document.querySelector('.pairs_list')
+    list: document.querySelector('.pairs_list'),
+    clearBtn: document.querySelector('#clear_button'),
+    sortByValueBtn: document.querySelector('.sort_value'),
+    sortByNameBtn: document.querySelector('.sort_name')
 }
-
-const arrOfPairs = []
+ 
+let arrOfPairs = []
+let arrForTrim = []
 
 refs.addBtn.addEventListener('click', addPair)
+refs.clearBtn.addEventListener('click', clearList)
+refs.sortByValueBtn.addEventListener('click', sortByValue)
+refs.sortByNameBtn.addEventListener('click', sortByName)
+
 
 const createListItem = (text) => `<li class="pairs-list_item">${text}</li>`
 
 const addLiToList = (markdown) => {
     refs.list.insertAdjacentHTML('beforeend', markdown)
 }
-// "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-// 
+
+function clearList(e) {
+  refs.list.innerHTML = ''
+  arrOfPairs = []
+  console.log(arrOfPairs)
+}
+ 
 function addPair(e) {
     e.preventDefault() 
-    const letters = /^[a-zA-Z]+[a-zA-Z]*=(?=[a-zA-Z])/;
+    const letters = /^[a-zA-Z0-9\s]*=(?=[\sa-zA-Z0-9]+[a-zA-Z0-9]$)/;
     if(!refs.input.value) {
         return
     }else if(refs.input.value.match(letters))
       {
-        const value = refs.input.value
-        const liItem = createListItem(value)
-        arrOfPairs.push(value)
-        console.log(arrOfPairs)
+
+        // delete all spaces between "=" and "Name/Value" pair
+        const value = refs.input.value.trim()
+        arrForTrim.push(value)
+        const trimArr = arrForTrim.map(el => el.split(' ').join('').split('  '))
+        const newArr = trimArr.flatMap(el => el)
+        let trimmedName = newArr[0]
+        
+        // create list item and push it into list
+        const liItem = createListItem(trimmedName)
+        arrOfPairs.push(trimmedName)
         addLiToList(liItem)
         refs.input.value = ''
+        arrForTrim = []
       }
     else
       {
-      alert("message");
+      alert("Write 'Name=Value' pair. Names and Values can contain only alpha-numeric characters.");
       return false;
       }
-   
-   
-      
+       
 }
 
-const sortByValueBtn = document.querySelector('.sort_value')
-const sortByNameBtn = document.querySelector('.sort_name')
- 
-sortByValueBtn.addEventListener('click', sortByValue)
-sortByNameBtn.addEventListener('click', sortByName)
+// Leonit = Augtin
+// Antony = Boris
+// Boris =   Johnson
+// Kilo = Mike
+// Yankee = Victor
+// Frank =  Antony
+// Echo = Bravo
+// Delta =  Charlie
+
+/* 
+ * Sorting list by Value. Reversing the array two times
+*/
+
 
 function sortByValue(e) {
     refs.list.innerHTML = ''
+    if(arrOfPairs) {
+      const sortedByValue = arrOfPairs.map(el => el.split('=').reverse().join('='))
+       
+      sortedByValue.sort((a, b) => a.localeCompare(b))
+      const sortedBack =  sortedByValue.map(el => el.split('=').reverse().join('='))
+      const markdown = sortedBack.reduce((acc, text) => acc + createListItem(text), '')
+      addLiToList(markdown)
+    }
     
-    const sortedByValue = arrOfPairs.map(el => el.split('=').reverse().join('='))
-    sortedByValue.sort((a, b) => a.localeCompare(b))
-    const sortedBack =  sortedByValue.map(el => el.split('=').reverse().join('='))
-    const markdown = sortedBack.reduce((acc, text) => acc + createListItem(text), '')
-    addLiToList(markdown)
-    console.log(sortedBack)
    
 }
 
- function sortByName (e) {
+/* 
+ * Sorting list by Name 
+*/
+
+
+ function sortByName () {
     refs.list.innerHTML = ''
-    console.log(arrOfPairs)
-    const sortedByName = arrOfPairs.sort((a, b) => a.localeCompare(b))  
-    const markdown = sortedByName.reduce((acc, text) => acc + createListItem(text), '')
-    addLiToList(markdown)
-    console.log(sortedByName)
+    if(arrOfPairs) {
+      const sortedByName = arrOfPairs.sort((a, b) => a.localeCompare(b))  
+      const markdown = sortedByName.reduce((acc, text) => acc + createListItem(text), '')
+      addLiToList(markdown)
+    }
+     
 }
